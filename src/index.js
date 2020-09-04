fcb5545e8ed41df90d1c0d098102ae74394570d9
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-04 20:19:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let submit = document.getElementById('submitBtn');
+  submit.addEventListener("click", () => {
+
+    let tasks = document.getElementById('tasks'),
+        li = document.createElement('li'),
+        newTask = document.getElementById('new-task-description'),
+        doneBtn = document.createElement('button');
+
+    tasks.appendChild(li);
+
+
+    li.innerHTML = newTask.value;
+    doneBtn.innerHTML = 'X';
+    doneBtn.setAttribute('data-description', newTask.value);
+    li.appendChild(doneBtn);
+    newTask.value = "";
+    event.preventDefault();
+
+    doneBtn.setAttribute('id', 'rmvTask');
+    document.getElementById("rmvTask").addEventListener("click", () => {
+      li.remove();
+      event.preventDefault();
+
+    });
+  });
 });
